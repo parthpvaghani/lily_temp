018158ef82352d7970ec2d19f933492a62199794
Parth Vaghani <parthvaghani14@gmail.com>
2022-07-25 13:39:25 +0530

    design first part

--- src/pages/WordBank.js
+++ src/pages/WordBank.js
@@ -14,6 +14,11 @@ import {
   StackDivider,
   HStack,
   Text,
+  Input,
+  Select,
+  PinInput,
+  PinInputField,
+  Button,
 } from '@chakra-ui/react';
 import { SearchIcon, QuestionOutlineIcon } from '@chakra-ui/icons';
 
@@ -72,23 +77,101 @@ export default function WordBank() {
             mt={3}
             px={6}
           >
-            <Box h="auto" w="33%">
+            <Box h="auto" w="33%" px={1}>
               <Box h="45px" bg="#E6E6FA" pt="8px">
                 Phonétiques
               </Box>
-              <Text py={4}>Son d'attaque: </Text>
+
+              <Box textAlign="left">
+                <Text mt={4}>Son d'attaque: </Text>
+                <Input borderColor={'gray.400'} mt={2} />
+
+                <Text mt={4}>Avec le(s) son(s): </Text>
+                <Input borderColor={'gray.400'} mt={2} />
+
+                <Text mt={4}>Sans le(s) son(S): </Text>
+                <Input borderColor={'gray.400'} mt={2} />
+
+                <Text mt={4}>Rime avec: </Text>
+                <Input borderColor={'gray.400'} mt={2} />
+
+                <Select
+                  placeholder="Nombre de syllabes phonétiques"
+                  borderColor={'gray.400'}
+                  mt={7}
+                >
+                  <option value="1">1 </option>
+                  <option value="2">2 </option>
+                  <option value="3">3 </option>
+                  <option value="4">4 </option>
+                </Select>
+              </Box>
             </Box>
-            <Box h="auto" w="33%">
+
+            <Box h="auto" w="33%" px={1}>
               <Box h="45px" bg="#E6E6FA" pt="8px">
                 Orthographiques
               </Box>
             </Box>
-            <Box h="auto" w="34%">
+
+            <Box h="auto" w="34%" px={1}>
               <Box h="45px" bg="#E6E6FA" pt="8px">
                 Sémantiques & grammaticaux
               </Box>
             </Box>
           </HStack>
+
+          <Text mt={12}>Forme du mot (Consonnes/Voyelles):</Text>
+          <Flex justifyContent={'center'} mt={3}>
+            <HStack>
+              <PinInput type="alphanumeric" otp>
+                <PinInputField borderColor={'gray.400'} />
+                <PinInputField borderColor={'gray.400'} />
+                <PinInputField borderColor={'gray.400'} />
+                <PinInputField borderColor={'gray.400'} />
+                <PinInputField borderColor={'gray.400'} />
+                <PinInputField borderColor={'gray.400'} />
+              </PinInput>
+            </HStack>
+          </Flex>
+
+          <Box mt={4}>
+            <Button
+              backgroundColor={'gray.600'}
+              color={'white'}
+              size="xs"
+              width={'7%'}
+              mx={4}
+            >
+              C
+            </Button>
+            <Button
+              backgroundColor={'gray.600'}
+              color={'white'}
+              size="xs"
+              width={'7%'}
+            >
+              V
+            </Button>
+          </Box>
+
+          <Flex justifyContent={'center'}>
+            <Select
+              placeholder="Nombre de lettres"
+              borderColor={'gray.400'}
+              mt={5}
+              width={'30%'}
+            >
+              <option value="1">1 </option>
+              <option value="2">2 </option>
+              <option value="3">3 </option>
+              <option value="4">4 </option>
+            </Select>
+          </Flex>
+
+          <Button backgroundColor={'gray.600'} color={'white'} mt={7} mb={10}>
+            Rechercher
+          </Button>
         </TabPanel>
       </TabPanels>
     </Tabs>
