be475cae9e1c332831c8fad41d1401764f04b736
Parth Vaghani <parthvaghani14@gmail.com>
2022-07-25 14:29:37 +0530

    logo changes

--- src/components/ui/Logo.js
+++ src/components/ui/Logo.js
@@ -4,7 +4,8 @@ import { Box, Image } from "@chakra-ui/react";
 export default function Logo(props) {
   return (
     <Box {...props}>
-       <Image src='LOGO-DSI-Horizontal.png' alt='DSI Logo' width={200}
+       <Image src='lilylearn_logo.png' alt='DSI Logo' width={100}
+       style={{filter:'invert(100%)'}}
        />
     </Box>
   );
